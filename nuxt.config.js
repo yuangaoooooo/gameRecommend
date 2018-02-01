2ab7ef309e996245b3f6744782ac29cd0decb158
gy <1198929918@qq.com>
2018-02-01 22:17:39 +0800

    游戏推荐项目初始化

---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  // 插件
+  // plugins:[
+  //   {src:'~/plugins/swiper-plugin.js',ssr:false}
+  // ],
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: '游戏推荐',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/css/main.css','~/assets/css/normalize.css',
+  '~/assets/css/font-awesome.css','swiper/dist/css/swiper.css','~/assets/css/animate.css'],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    vendor: ['axios'],
+    /*
+    ** Run ESLint on save
+    */
+    // extend (config, { isDev, isClient }) {
+    //   if (isDev && isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+      <i class="fa fa-user"></i>
+    </div>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+  
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
